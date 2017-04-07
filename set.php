2fb326f94f2dcec4c31ea0896b228c1574120373
sadri90125449 <sadriwensa@gmail.com>
2017-04-07 11:53:50 +0700

    Create set.php

---
+++ set.php
@@ -0,0 +1,19 @@
+<?php
+// Load composer
+require __DIR__ . '/vendor/autoload.php';
+
+$API_KEY = '227029803:AAH7-Jh8UJ7OL1mfHn_6NwvT-EiEWzwFDhE';
+$BOT_NAME = '@realisasimantritkn_bot';
+$hook_url = 'https://api.telegram.org/bot227029803:AAH7-Jh8UJ7OL1mfHn_6NwvT-EiEWzwFDhE/sadri/set.php'
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
+
+    // Set webhook
+    $result = $telegram->setWebhook($hook_url);
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    echo $e;
+}
